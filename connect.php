e1bce09ff8b1012b320b270ac984078d53194759
Nishan Ahmed <nishanahmad@gmail.com>
2025-11-01 10:56:20 +0530

    Stop tracking connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php	
-	$con=mysqli_connect("localhost","nishan","darussalam123.","fitness");
-	// Check connection
-	if ($con->connect_error) 
-	{
-		die("Connection failed: " . $con->connect_error);
-	}
-	///////HIiiii
-?>
